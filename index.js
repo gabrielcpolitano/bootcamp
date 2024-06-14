0455e8361ca1c922ab82fac78909b35587cd6ebc
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-06-14 08:28:16 -0300

    Thanks God

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+//ROTA
+  // Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
+    // GET - Pega uma info
+    // POST - Cria uma info
+    // PUT - Altera toda a info
+    // PATH - Altera parte da info
+    // DELETE - Apaga uma info
+
+  // Name - Um identificador da rota
+
+  // Function (Callback) - Respondavel por executar algum comando
+
+app.get("/soma", (req, res) => {
+  const soma = 1 + 202
+
+  res.send({soma: soma})
 })
 
 app.listen(3000)
